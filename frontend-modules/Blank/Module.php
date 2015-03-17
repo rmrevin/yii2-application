413db0a4f353f93f16e42d000ddecd36eeb2e8dd
Revin Roman <xgismox@gmail.com>
2015-03-17 13:54:17 +0300

    Update

---
+++ frontend-modules/Blank/Module.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Module.php
+ * @author Revin Roman http://phptime.ru
+ */
+
+namespace frontend\modules\Blank;
+
+/**
+ * Class Module
+ * @package frontend\modules\Blank
+ */
+class Module extends \yii\base\Module
+{
+
+}
