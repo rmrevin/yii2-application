b1b0b7eb0ac4a5dfde3f92ea6e9b03022a872c43
Revin Roman <xgismox@gmail.com>
2015-03-17 23:37:43 +0300

    Update

--- resources/User/Auth/AbstractSocial.php
+++ resources/User/Auth/AbstractSocial.php
@@ -4,11 +4,11 @@
  * @author Revin Roman http://phptime.ru
  */
 
-namespace resources\User;
+namespace resources\User\Auth;
 
 /**
  * Class AbstractSocial
- * @package resources\User
+ * @package resources\User\Auth
  *
  * @property integer $user_id
  * @property string $social_id
