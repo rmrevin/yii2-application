e3dbecfaa436ddcbe3fbe4714fd9667f37146c4e
Revin Roman <xgismox@gmail.com>
2015-05-15 23:00:30 +0300

    Update

--- frontend-app/migrations/m150317_103004_account_init.php
+++ frontend-app/migrations/m150317_103004_account_init.php
@@ -5,7 +5,7 @@ use yii\db\mysql\Schema;
 class m150317_103004_account_init extends \common\components\Migration
 {
 
-    static $socials = ['facebook', 'github', 'google', 'linkedin', 'live', 'twitter', 'vkontakte', 'yandex'];
+    static $socials = [];
 
     public function safeUp()
     {
@@ -29,34 +29,39 @@ class m150317_103004_account_init extends \common\components\Migration
         $this->createIndex('idx_token', '{{%user}}', 'token');
         $this->createIndex('idx_auth_key', '{{%user}}', 'auth_key');
 
-        $this->createTable('{{%user_auth_response}}', [
-            'id' => Schema::TYPE_PK,
-            'received_at' => Schema::TYPE_INTEGER,
-            'client' => Schema::TYPE_STRING,
-            'response' => Schema::TYPE_TEXT,
-            'result' => Schema::TYPE_TEXT,
-            'user_ip' => Schema::TYPE_BIGINT,
-        ]);
-
-        $this->createIndex('idx_received_at', '{{%user_auth_response}}', ['received_at']);
-
-        foreach (static::$socials as $social) {
-            $this->createTable('{{%user_auth_' . $social . '}}', [
-                'user_id' => Schema::TYPE_INTEGER,
-                'social_id' => Schema::TYPE_STRING,
-                'PRIMARY KEY (user_id,social_id)',
-                'FOREIGN KEY (user_id) REFERENCES {{%user}} (id) ON DELETE CASCADE ON UPDATE CASCADE',
+        if (!empty(static::$socials)) {
+            $this->createTable('{{%user_auth_response}}', [
+                'id' => Schema::TYPE_PK,
+                'received_at' => Schema::TYPE_INTEGER,
+                'client' => Schema::TYPE_STRING,
+                'response' => Schema::TYPE_TEXT,
+                'result' => Schema::TYPE_TEXT,
+                'user_ip' => Schema::TYPE_BIGINT,
             ]);
+
+            $this->createIndex('idx_received_at', '{{%user_auth_response}}', ['received_at']);
+
+            foreach (static::$socials as $social) {
+                $this->createTable('{{%user_auth_' . $social . '}}', [
+                    'user_id' => Schema::TYPE_INTEGER,
+                    'social_id' => Schema::TYPE_STRING,
+                    'PRIMARY KEY (user_id,social_id)',
+                    'FOREIGN KEY (user_id) REFERENCES {{%user}} (id) ON DELETE CASCADE ON UPDATE CASCADE',
+                ]);
+            }
         }
     }
 
     public function safeDown()
     {
-        foreach (static::$socials as $social) {
-            $this->dropTable('{{%user_auth_' . $social . '}}');
+        if (!empty(static::$socials)) {
+            foreach (static::$socials as $social) {
+                $this->dropTable('{{%user_auth_' . $social . '}}');
+            }
+
+            $this->dropTable('{{%user_auth_response}}');
         }
 
-        $this->dropTable('{{%user_auth_response}}');
         $this->dropTable('{{%user}}');
     }
 }
